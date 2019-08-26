f12e571ec7a4c0ad0d6c29feeb400b4bee3910bb
karolwalasik <karol.walasik@gmail.com>
2019-08-26 17:18:01 +0200

    add volume control

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,6 +24,11 @@ function updateTimeProgressBar(audio) {
     $(".playbackBar .progress").css("width", progress + "%");
 }
 
+function updateVolumeProgressBar(audio) {
+    var volume = audio.volume * 100
+    $(".volumeBar .progress").css("width", volume + "%");
+}
+
 function Audio() {
     this.currentlyPlaying;
     this.audio = document.createElement('audio');
@@ -31,12 +36,17 @@ function Audio() {
     this.audio.addEventListener("canplay", function () {
         var duration = formatTime(this.duration)
         $(".progressTime.remaining").text(duration);
+
     });
 
     this.audio.addEventListener("timeupdate", function () {
         if (this.duration) {
             updateTimeProgressBar(this);
         }
+    });
+
+    this.audio.addEventListener("volumechange", function () {
+        updateVolumeProgressBar(this);
     });
 
     this.setTrack = function (track) {
